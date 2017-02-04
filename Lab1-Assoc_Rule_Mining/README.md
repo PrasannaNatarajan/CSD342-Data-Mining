4680f67db616064977a8cba1ec980983b4be19c7
FlameFractal <vishg8@gmail.com>
2017-02-05 03:00:45 +0530

    Added simulated transactions generator

--- Lab1-Assoc_Rule_Mining/README.md
+++ Lab1-Assoc_Rule_Mining/README.md
@@ -3,48 +3,46 @@
 ### Table of contents
 
 1. [Generate random items (Zipf-ian)](#generate-random-items)
-2. [Generate random transactions](#generate-random-transactions)
-3. [Apriori Rule](#apriori-rule)
-4. [Visualization](#visualization)
-5. [README and Documentation](#readme-and-documentation)
 
-<br><br>
+2. [Generate random transactions (Normally distributed)](#generate-random-transactions)
+
+3. [Apriori Rule](#apriori-rule)
+
+4. [Visualization](#visualization)
+
+5. [Documentation](#documentation)
+
+   <br>
 
 ## Generate random items
 
-- (Zipf distribution / Pareto principle)
+-   Quantities should be in Zipf distribution (Pareto principle)
 
-- Frequency of items. Begin with 5 - 10 items but demo with a lot more (10000)
+-   Frequency of items. Begin with 5 - 10 items but demo with a lot more (10,000)
 
-- Steps to generate
+-   | Item | Frequency |
+    | ---- | --------- |
+    | a    | 1         |
+    | b    | 2         |
+    | c    | 3         |
+    | d    | 3         |
 
-  - create n buckets for n items 
-
-  - define a parameter for total frequency of all items
-
-    - Eg. {a,b,c,d}, {b,c,d}, {c,d}, parameter (all freq) = 9
-
-    - | Item | Frequency |
-      | ---- | --------- |
-      | a    | 1         |
-      | b    | 2         |
-      | c    | 3         |
-      | d    | 3         |
-
-  - zipf factor - 0 < f < 1 , **use 0.5**
-
+-   Zipf factor - 0 < f < 1 , **use 0.5**
     - higher the factor - more skewed the distribution
-      lower the factor - well distributed uniform sample
+    - lower the factor - well distributed uniform sample
 
 ## Generate random transactions
 
-- generate random number for **num_items** in transaction **i**
-- generate random number (id) to decide which items to include in a transcation
+- Number of items in transaction
+  - Generate randomly but normally distributed. Most people will buy 3-7 items in case of 0 to 10 items (max range)
+- Generate random number (id) to decide which items to include in that transcation
   - check if any one item's frequency is 0 before picking up
   - check if total frequency is less than num_items (in the end)
-- pick up from the item table {decrement frequency by 1}
+- Pick up from the item table {decrement frequency by 1}
 
 ## Apriori Rule
+
+- Apply the apriori principle in generating candiate itemsets.
 
 ## Visualization
 
@@ -58,7 +56,9 @@
 
   - Don't show more than 5-10 itemsets
 
-## README and Documentation
+## Documentation
 
+- Show flags with default values
 - assoc.java -n {num_items=default_val} -s {support_threshold=default_val}
-- Well commented code! Along with {name - roll no} as a comment on top of the code
+- Well commented code along with {name - roll no} as a comment on top of the code
+
--- Lab1-Assoc_Rule_Mining/apriori.java
+++ Lab1-Assoc_Rule_Mining/apriori.java
@@ -3,130 +3,157 @@ import java.util.*;
 public class apriori {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Set<Integer> set = new HashSet<Integer>();
-		int n;
-		System.out.println("Enter the size of the set: ");
-		Scanner scanner = new Scanner(System.in);
-		n = scanner.nextInt();
-		try{
 			
-			for(int i=0;i<n;i++){
-				set.add(i%5);				
-			}
-		Set<Set<Integer>> power1 = powerSet1(set);
+	 	/* Auto mode, default values */
+	 	ItemsGenerator items = new ItemsGenerator(100, 0.05 , 100000);
+ 		TransactionsGenerator transactions = new TransactionsGenerator(items,10);
 		
-		}catch(Exception e){
-			System.out.println(" Try failed");
-			e.printStackTrace();
-		}
-		
-		scanner.close();
-		
-	}
-		
-	public static <T> Set<Set<T>> powerSet1(Set<T> originalSet){
-		Set<Set<T>>s = new HashSet<Set<T>>();
-		T a[] = (T[]) originalSet.toArray();
-		Set<T> tempSet = new HashSet<T>();
-		int counter = (int)Math.pow(2, originalSet.size());
-		for(int i=0;i<counter;i++){
-			for(int j=0;j<originalSet.size();j++){
-				if(((i) & (1<<j)) != 0){
-					System.out.print(a[j]+" ");
-					tempSet.add(a[j]);
-				}
-			}
-			s.add(tempSet);
-			System.out.println("");
-			tempSet.clear();
-		}
-		System.out.println("done");
-		return s;
+		/* Unit Test - Simulating items and transactions generation */
+		/*
+		for (int i = 0; i < transactions.transactions.size(); i++) {
+	        System.out.print((i+1)+". ");
+			// System.out.print((transactions.transactions.get(i).size()));
+	        for (int j = 0; j < transactions.transactions.get(i).size(); j++) {
+	            System.out.print(transactions.transactions.get(i).get(j)+" ");
+	        }
+	 		System.out.println();
+	 	}
+	 	*/
+
+
 	}
 
+
+/*-------------------------------------------------------------------------------------------------------------------
+DESCRIPTION: 
+Class to generate simulated transactions with normally distributed number of items in each transaction.
+
+USAGE: TransactionsGenerator transactions = new TransactionsGenerator(ItemsGenerator items, int num_item_range);
+EXAMPLE: TransactionsGenerator transactions = new TransactionsGenerator(items, 5);
+
+items = ItemsGenerator object containing generated possible items.
+num_item_range = upper bound of nmber of items to generate in one transaction.
+
+transactions.transactions = List of Lists containing items of each transaction as a row.
+----------------------------------------------------------------------------------------------------------------------*/
+
+class TransactionsGenerator {
+
+	private ItemsGenerator items;
+	private int num_items_range;
+
+	List<List<Integer>> transactions; 
+
+	public TransactionsGenerator(ItemsGenerator items, int num_items_range){
+		this.items = items;
+		this.num_items_range = num_items_range;
+		this.createTransactions();
+	};
+
+	void createTransactions(){
+		this.transactions = new ArrayList<>();
+		int i = 0;
+
+		while(i < this.items.generated_quantity){ /* Until quantity of all items is exhausted, keep generating transactions */
+			int num_items; 
+
+			do { /* Find a random (normally distributed) number of items in a transaction within 0 to num_items_range */
+				num_items = (int) Math.round(new Random().nextGaussian() * (3) + (num_items_range/2));
+			} while (num_items <= 0 || num_items >=num_items_range);
+
+			if(num_items > this.items.generated_quantity-i){ /* If less items are left than required, set required to what's left. */
+				num_items = this.items.generated_quantity-i;
+			}
+
+			this.transactions.add(new ArrayList<Integer>(num_items));
+
+			for (int j = 0; j < num_items; j++) {
+            	// int search_threshold = (this.items.quantity); /* number of items to randomly search till non-zero frequency item is found */
+				int pick_item_id = ((int)(Math.random()*this.items.size));
+				while((int)(items.frequency_map.get(pick_item_id)) <= 0){
+					pick_item_id = ((int)(Math.random()*this.items.size));
+				}
+				this.transactions.get(this.transactions.size()-1).add(pick_item_id);
+				this.items.frequency_map.set(pick_item_id, this.items.frequency_map.get(pick_item_id) - 1); 
+				i++;
+			}
+		}
+	}
 }
 
 
-//-------------------------------------------------------------------------------
-
-/* 
-
-Usage: ItemsGenerator items = new ItemsGenerator(size, skew, quantity);
-Example: ItemsGenerator items = new ItemsGenerator(50, 0.5, 1000);
-
-size = number of unique items
-skew = zipf skew. Lies between 0 and 1.
-quantity = total quantity of all items inclusive
-
-
-
-Description: 
-Class `ItemsGenerator` to generate N*Q number of items as a HashMap (item_id -> quantity)
+/*-------------------------------------------------------------------------------------------------------------------
+DESCRIPTION: 
+Class to generate simulated dataset containing N*Q number of items as an ArrayList[item_id] = quantity.
 
 N = number of items
-Q = total quantity of items
+Q = total quantity of all items
 
-By default approx ~100 total quantity is generated.
-Each item's quantity is scaled up by factor of Q/100 to get Q total quantity.
+USAGE: ItemsGenerator items = new ItemsGenerator(size, skew, quantity);
+EXAMPLE: ItemsGenerator items = new ItemsGenerator(50, 0.5, 1000);
 
-*/
+frequency_map is ArrayList containing frequency of item index i.
+
+size = number of unique items
+skew = zipf skew. 0 -> equally distributed. 10 -> opposite.
+quantity = total quantity of all items inclusive
+-------------------------------------------------------------------------------------------------------------------*/
 
 class ItemsGenerator {
- private Random rnd = new Random(System.currentTimeMillis());
- private int size;
- private int quantity;
- private double skew;
- private double bottom = 0;
- Map frequency_map;
+	private Random rnd = new Random(System.currentTimeMillis());
+	public int size;
+	public int generated_quantity;
+	public int quantity;
+	private double skew;
+	private double bottom = 0;
+	ArrayList<Integer> frequency_map;
 
- public ItemsGenerator(int size, double skew, int quantity) {
+	public ItemsGenerator(int size, double skew, int quantity) {
 
-  this.size = size;
-  this.skew = skew;
-  this.quantity = quantity;
+		this.size = size;
+		this.skew = skew;
+		this.quantity = quantity;
 
-  for (int i = 1; i < size; i++) {
-   this.bottom += (1 / Math.pow(i, this.skew));
-  }
-  
-  this.createItemsMap();
+		for (int i = 1; i < size; i++) {
+			this.bottom += (1 / Math.pow(i, this.skew));
+		}
 
- }
+		this.createItemsMap();
+	}
 
- //The next() method returns an random rank id whose frequency follows Zipf distribution.
- public int next() {
-  int rank;
-  double frequency;
-  double dice;
+ /* The next() method returns an random rank id whose frequency follows Zipf distribution. */
+	public int next() {
+		int rank;
+		double frequency;
+		double dice;
 
-  rank = rnd.nextInt(size);
-  frequency = (1.0d / Math.pow(rank, this.skew)) / this.bottom;
-  dice = rnd.nextDouble();
+		rank = rnd.nextInt(size);
+		frequency = (1.0d / Math.pow(rank, this.skew)) / this.bottom;
+		dice = rnd.nextDouble();
 
-  while (!(dice < frequency)) {
-   rank = rnd.nextInt(size);
-   frequency = (1.0d / Math.pow(rank, this.skew)) / this.bottom;
-   dice = rnd.nextDouble();
-  }
+		while (!(dice < frequency)) {
+			rank = rnd.nextInt(size);
+			frequency = (1.0d / Math.pow(rank, this.skew)) / this.bottom;
+			dice = rnd.nextDouble();
+		}
 
-  return rank;
- }
+		return rank;
+	}
 
- //This method returns a probability that the given rank occurs.
- public double getProbability(int rank) {
-  return (1.0d / Math.pow(rank, this.skew)) / this.bottom;
- }
+ /* This method returns a probability that the given rank occurs. */
+	public double getProbability(int rank) {
+		return (1.0d / Math.pow(rank, this.skew)) / this.bottom;
+	}
 
- void createItemsMap(){
-  int i, prob, sum = 0;
-  frequency_map = new HashMap();
-  for (i = 1; i <= this.size; i++) {
-   prob = (int)(Math.round(this.getProbability(i) * 100));
-   frequency_map.put(i, prob*(this.quantity/100));
-   // System.out.println(i + " " + prob);
-   sum = sum + prob;
-  }
-  // System.out.println("sum=" + sum);
- }
+	void createItemsMap(){
+		int i, quant_item;
+		frequency_map = new ArrayList();
+		for (i = 1; i <= this.size; i++) {
+			quant_item = (int)((this.getProbability(i))*(this.quantity));
+			frequency_map.add(i-1, quant_item);
+			System.out.println(i-1 + " " + quant_item);
+			generated_quantity = generated_quantity + quant_item;
+		}
+		System.out.println("generated_quantity =" + generated_quantity);
+	}
 }
