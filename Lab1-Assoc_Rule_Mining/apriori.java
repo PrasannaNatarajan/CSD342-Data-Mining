4d4cfbbdb0b4cdb7abc1f5c6a9432d6c3c9a4d89
= <prasanna2431997@gmail.com>
2017-02-06 00:40:34 +0530

    done apriori code

--- Lab1-Assoc_Rule_Mining/apriori.java
+++ Lab1-Assoc_Rule_Mining/apriori.java
@@ -1,16 +1,22 @@
 import java.util.*;
 
-public class apriori {
+public class lab1 {
 
 	public static void main(String[] args) {
-			
-	 	/* Auto mode, default values */
-	 	ItemsGenerator items = new ItemsGenerator(100, 0.05 , 100000);
- 		TransactionsGenerator transactions = new TransactionsGenerator(items,10);
+		// TODO Auto-generated method stub
 		
-		/* Unit Test - Simulating items and transactions generation */
-		/*
-		for (int i = 0; i < transactions.transactions.size(); i++) {
+		ItemsGenerator items = new ItemsGenerator(10, 0.5 , 50);
+ 		TransactionsGenerator transactions = new TransactionsGenerator(items,10);
+ 		ArrayList<Integer> new_inp[] = new ArrayList[transactions.transactions.size()];
+ 		
+ 		for(int i=0;i<transactions.transactions.size();i++){
+ 			new_inp[i] = new ArrayList<Integer>();
+ 			for(int j=0;j<transactions.transactions.get(i).size();j++){
+ 				new_inp[i].add(j,transactions.transactions.get(i).get(j));
+ 			}
+ 			
+ 		}
+ 		for (int i = 0; i < transactions.transactions.size(); i++) {
 	        System.out.print((i+1)+". ");
 			// System.out.print((transactions.transactions.get(i).size()));
 	        for (int j = 0; j < transactions.transactions.get(i).size(); j++) {
@@ -18,24 +24,164 @@ public class apriori {
 	        }
 	 		System.out.println();
 	 	}
-	 	*/
-
-
+ 		
+		ArrayList<Integer> inp[] = new ArrayList[5];
+		for(int i=0;i<5;i++){
+			inp[i] = new ArrayList<Integer>();
+			for(int j=0;j<(int)(Math.random()*10);j++){
+				inp[i].add((int)(Math.random()*10));
+				System.out.print(inp[i].get(j)+ " ");
+			}
+			System.out.println("");
+		}
+		float s = 5;
+		
+		ArrayList<ArrayList<Integer>> ans = algo(new_inp,s,10);
+		
+		for(int i=0;i<ans.size();i++){
+			System.out.println("size: "+ans.get(i).size());
+			for(int j=0;j<ans.get(i).size();j++){
+				System.out.print(ans.get(i).get(j) + " ");
+			}
+			System.out.println("");
+		}
+		
+		System.out.println("done");
+		
+		
 	}
+	
+	public static ArrayList<ArrayList<Integer>> algo(ArrayList<Integer>[] inp,float threshold, int num){
+		
+		int index_counter =  0;
+		int max = (int)Math.pow(2, num);
+		int c[] = new int[num];
+		for(int i=0;i<num;i++){
+			c[i]=0;
+		}
+		
+		ArrayList<ArrayList<Integer>> selected1 = new ArrayList<ArrayList<Integer>>();
+		
+		ArrayList<Integer> uniqueElements = new ArrayList(num);
+		for(int i=0;i<num;i++){
+			uniqueElements.add(i,i);//[i]=i;
+		}		
+		//////////////////////////////////////////////////////////
+		int i=0;
+		ArrayList<Integer> temp = new ArrayList<>();
+		while(i<num){
+			
+			for(int j=0;j<inp.length;j++){
+				if(inp[j].contains(i)){
+					c[i]++;
+					System.out.println("The count of "+i+" is:"+c[i]);
+				}
+			}
+			if(c[i]>=threshold){
+				temp.add(i);
+			}
+			
+			System.out.println("temp.size = "+temp.size());
+			i++;
+		}
+		selected1.add(index_counter,temp);
+		index_counter++;
+		
+		for(int j=0;j<selected1.get(0).size();j++){
+			System.out.println("The selected items are: "+selected1.get(0).get(j));
+		}
+		
+		///////////////////////////////////////////////////////////
+		
+		
+		for(int k=2;k<6;k++){	
+			
+			int numSubsets = factorial(selected1.get(0).size()) / (factorial(selected1.get(0).size() - k) * factorial(k));
+			System.out.println("numSubsets = "+numSubsets);
+			int[] counter = new int[numSubsets];
+			for(int p=0;p<numSubsets;p++)counter[p]=0;
+			
+			int[] s = new int[k];
+			ArrayList<Integer>[] subsets = getSubsetWrapper(numSubsets,selected1.get(0),s,k);
+			
+			for(int q=0;q<inp.length;q++)
+			for(int p = 0;p<numSubsets;p++){
+				//System.out.println("subsets = "+subsets[p]);
+				//System.out.println("inp["+q+"] = "+inp[q]);
+				//System.out.println("inp[q].containsAll(subsets[p]) = "+subsets[p].containsAll(inp[q]));
+				if(inp[q].containsAll(subsets[p])){
+					counter[p]++;
+					//System.out.println("counter["+p+"] = "+counter[p]);
+				}
+				if(counter[p]>=threshold){
+					selected1.add(index_counter,subsets[p]);
+					index_counter++;
+				}
+			}			
+		}
+		
+		//for(int j=0;j<index_counter;j++){
+			//System.out.println("j = "+j+" "+selected1.get(j).size());
+		//}
+			
+		
+		return selected1;
+		
+	}
+	
+	
+    public static int factorial(int fac) {
+        if (fac <= 1) return 1;
+        
+        return (int) (fac * factorial(fac - 1));        
+    }
+	
+	public static ArrayList<Integer>[] getSubsetWrapper(int numSubsets,ArrayList<Integer> input, int[] subset,int k){
+		ArrayList<Integer>[] subsets = new ArrayList[numSubsets];
+		for(int i=0;i<numSubsets;i++){
+			subsets[i] = new ArrayList<Integer>();
+		}
+		int[] s = new int[k];                  // here we'll keep indices 
+		int index_counter= 0;                  // pointing to elements in input array
+		
+		if (k <= input.size()) {
+		    // first index sequence: 0, 1, 2, ...
+		    for (int i = 0; (s[i] = i) < k - 1; i++);  
+		    
+		    subsets[index_counter]= getSubset(input, s);
+		    index_counter++;
+		    
+		    for(;;) {
+		        int i;
+		        // find position of item that can be incremented
+		        for (i = k - 1; i >= 0 && s[i] == input.size() - k + i; i--); 
+		        if (i < 0) {
+		            break;
+		        } else {
+		            s[i]++;                    // increment this item
+		            for (++i; i < k; i++) {    // fill up remaining items
+		                s[i] = s[i - 1] + 1; 
+		            }
+		            //subsets.add(getSubset(input, s));
+		            subsets[index_counter] = getSubset(input,s);
+		            index_counter++;
+		        }
+		    }
+		}		
+		
+		return subsets;
+	}
+	
+	// generate actual subset by index sequence
+	public static ArrayList<Integer> getSubset(ArrayList<Integer> input, int[] subset) {
+	    ArrayList<Integer> res = new ArrayList(subset.length);
+	    for (int i = 0; i < subset.length; i++) 
+	    	res.add(i,input.get(subset[i]));
+	        
+	    return res;
+	}	
 
-
-/*-------------------------------------------------------------------------------------------------------------------
-DESCRIPTION: 
-Class to generate simulated transactions with normally distributed number of items in each transaction.
-
-USAGE: TransactionsGenerator transactions = new TransactionsGenerator(ItemsGenerator items, int num_item_range);
-EXAMPLE: TransactionsGenerator transactions = new TransactionsGenerator(items, 5);
-
-items = ItemsGenerator object containing generated possible items.
-num_item_range = upper bound of nmber of items to generate in one transaction.
-
-transactions.transactions = List of Lists containing items of each transaction as a row.
-----------------------------------------------------------------------------------------------------------------------*/
+}
 
 class TransactionsGenerator {
 
@@ -70,7 +216,7 @@ class TransactionsGenerator {
 			for (int j = 0; j < num_items; j++) {
             	// int search_threshold = (this.items.quantity); /* number of items to randomly search till non-zero frequency item is found */
 				int pick_item_id = ((int)(Math.random()*this.items.size));
-				while((int)(items.frequency_map.get(pick_item_id)) <= 0){
+				while((int)(items.frequency_map.get(pick_item_id)) <= 0 && (this.transactions.get(this.transactions.size()-1).contains(pick_item_id))){
 					pick_item_id = ((int)(Math.random()*this.items.size));
 				}
 				this.transactions.get(this.transactions.size()-1).add(pick_item_id);
@@ -85,15 +231,11 @@ class TransactionsGenerator {
 /*-------------------------------------------------------------------------------------------------------------------
 DESCRIPTION: 
 Class to generate simulated dataset containing N*Q number of items as an ArrayList[item_id] = quantity.
-
 N = number of items
 Q = total quantity of all items
-
 USAGE: ItemsGenerator items = new ItemsGenerator(size, skew, quantity);
 EXAMPLE: ItemsGenerator items = new ItemsGenerator(50, 0.5, 1000);
-
 frequency_map is ArrayList containing frequency of item index i.
-
 size = number of unique items
 skew = zipf skew. 0 -> equally distributed. 10 -> opposite.
 quantity = total quantity of all items inclusive
@@ -157,3 +299,4 @@ class ItemsGenerator {
 		System.out.println("generated_quantity =" + generated_quantity);
 	}
 }
+
