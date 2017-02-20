b81601459b5f520dfdaf06105b5bef7d49d3fef5
FlameFractal <vishg8@gmail.com>
2017-02-20 22:20:12 +0530

    Refactored Kmeans 1.indentation 2. final submit

--- Lab2-K_Means_Clustering/README - Lab 2 - KMeans.md
+++ Lab2-K_Means_Clustering/README - Lab 2 - KMeans.md
@@ -1,12 +1,8 @@
-# README - Lab 2 - KMeans
+# README - Lab 2 - K Means
 
-### **NAME**
+### **DESCRIPTION**
 
-K Means - K Means algorithm for Clustering.
-
-### **SYNOPSIS**
-
-This is an implementation of k-means algorithm for clustering in Java.
+K Means algorithm for Clustering in Java
 
 ### **DATASET**
 
@@ -25,25 +21,23 @@ All parameters are optional, if not specified, default values will be used.
 
 ### **OPTIONS**
 
-- k_clusters
+- k_clusters - Integer input to define number of clusters. 
 
-  - Integer input to define number of clusters. 
-    - Default=7.
-    - Tested Input Range= {0,100}
-- distance_algorithm
+  - Default=7.
+  - Tested Input Range= {0,100}
+- distance_algorithm - Integer input to define which distance algorithm to use.
 
-  - Integer input to define which distance algorithm to use.
-    - Default=0. 
-    - Input Range= {0,1}
-    - "0" stands for eucledian distance
-    - "1" stands for manhattan distance
+  - Default=0. 
+  - Input Range= {0,1}
+  - "0" stands for eucledian distance
+  - "1" stands for manhattan distance
 
 - file_path
 
   - Path of the data file.
 
 
-### **Example**
+### **EXAMPLE**
 
 `java Lab2 -k 7 -d 1 -f input.csv`
 
--- Lab2-K_Means_Clustering/src/Point.java
+++ Lab2-K_Means_Clustering/src/Point.java
@@ -1,41 +1,56 @@
-public class Point{
-	private double x;
-	private double y;
-	private int cluster_id;
-	
-	Point(double x, double y){
-		this.set_x(x);
-		this.set_y(y);
-		
-	}
-	
-	void set_x(double x){
-		this.x = x;
-	}
-	
-	void set_y(double y){
-		this.y = y;
-	}
-	
-	
-	double get_x(){
-		return this.x;
-	}
-	
-	double get_y(){
-		return this.y;
-	}
-	
-	public void setClusterId(int id){
-		this.cluster_id = id;
-	}
-	
-	public int getClusterId(){
-		return this.cluster_id;
-	}
-	
-	public String printPoint(){
-		return this.get_x()+", "+this.get_y();
-	}
-	
+/*
+ This is the submission for the graded lab assignment #2, which consists of implementation of k-means algorithm.
+ The algorithm has been implemented in Java v1.7.
+ This implementation has been successfully tested for up to 1000000 elements. 
+ Inputs  : the number of clusters, the dataset as a csv file and the distance metric.
+ Outputs : all the data points assigned to a cluster and a graph which shows the cluster heads and the points.
+
+ CONTRIBUTORS
+ Atish Majumdar      : 1410110081
+ Prasanna Natarajan  : 1410110298
+ Vishal Guaba        : 1410110501
+*/
+
+package lab2;
+
+public class Point {
+ private double x;
+ private double y;
+ private int cluster_id;
+
+ Point(double x, double y) {
+  this.set_x(x);
+  this.set_y(y);
+
+ }
+
+ void set_x(double x) {
+  this.x = x;
+ }
+
+ void set_y(double y) {
+  this.y = y;
+ }
+
+
+ double get_x() {
+  return this.x;
+ }
+
+ double get_y() {
+  return this.y;
+ }
+
+ public void setClusterId(int id) {
+  this.cluster_id = id;
+ }
+
+ public int getClusterId() {
+  return this.cluster_id;
+ }
+
+ public String printPoint() {
+  return this.get_x() + ", " + this.get_y();
+ }
+
 }
